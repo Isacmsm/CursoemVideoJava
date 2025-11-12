eef9be8a60e4af101510d7d3a32fa67e8c640fd1
Isac <isac.manoelsm@gmail.com>
2025-11-12 00:20:26 -0300

    renomeando pastas e novas pastas

---
+++ Aula2/Caneta.java
@@ -0,0 +1,31 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + cor);
+        System.out.println("Está tampada? " + this.tampada);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+    }
+
+    void rabiscar(){
+        if (this.tampada == true){
+            System.out.println("Não posso rabiscar, a caneta esta tampada!");
+        } else{
+            System.out.println("Estou rabiscando!");
+        }
+    }
+
+    void tampar(){
+        this.tampada = true;
+    }
+
+    void destampar(){
+        this.tampada = false;
+    }
+}
